016aaf3cb8186974745b82c3aa1d6db3e3beb386
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-02-10 07:38:23 +0100

    On loading, compile style again (in case db was re-initialized)

--- pgmapcss.php
+++ pgmapcss.php
@@ -24,6 +24,8 @@ function ajax_load($param) {
     return "WRONG ID!";
 
   $file = "{$id}.mapcss";
+  // in case we re-initialized the database, compile again
+  $f=adv_exec("pgmapcss -dskunk -uskunk -pPASSWORD -Hlocalhost -tmapnik-2.2 \"{$file}\"", "data/", array());
 
   return file_get_contents("data/{$file}");
 }
