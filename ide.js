46a6e6031d754f3d3e953a27208c59da3e9f538f
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-02-15 07:35:59 +0100

    IDE: remember current style id, so we do not reload constantly

--- ide.js
+++ ide.js
@@ -1,3 +1,5 @@
+var ide_current_style;
+
 register_hook("init", function() {
   // Bind IDE to function
   document.getElementById("ide").onsubmit = process_form;
@@ -24,9 +26,11 @@ function process_form(foo) {
 }
 
 register_hook("param_change", function(params) {
-  if('style' in params)
+  if(('style' in params) && (ide_current_style != params.style))
     ajax("load", { 'id': params.style }, null, function(data) {
       var form = document.getElementById("ide");
       form.elements.mapcss_file.value = data;
     });
+
+  ide_current_style = params.style;
 });
