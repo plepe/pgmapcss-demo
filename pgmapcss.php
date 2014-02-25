733caa220c23f137177ec730222b7ecc69588f12
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-02-25 14:30:13 +0100

    Configure parameters for database and pgmapcss

--- pgmapcss.php
+++ pgmapcss.php
@@ -4,14 +4,18 @@ function build_id($content) {
 }
 
 function ajax_save($param, $content) {
+  global $pgmapcss;
+  global $db;
+  global $data_dir;
+
   $id = build_id($content);
   $file = "{$id}.mapcss";
 
-  file_put_contents("data/{$file}", $content);
-  $f=adv_exec("pgmapcss -dskunk -uskunk -pPASSWORD -Hlocalhost -tmapnik-2.2 \"{$file}\"", "data/", array());
+  file_put_contents("{$data_dir}/{$file}", $content);
+  $f=adv_exec("{$pgmapcss['path']} -d'{$db['database']}' -u'{$db['user']}' -p'{$db['password']}' -H'{$db['host']}' -t'{$pgmapcss['template']}' '{$file}'", $data_dir, array());
 
   if($f[0] != 0) {
-    unlink("data/{$file}");
+    unlink("{$data_dir}/{$file}");
     $id = null;
   }
 
@@ -23,13 +27,17 @@ function ajax_save($param, $content) {
 }
 
 function ajax_load($param) {
+  global $pgmapcss;
+  global $db;
+  global $data_dir;
+
   $id = $param['id'];
   if(!preg_match('/^[0-9a-z]+$/i', $id))
     return "WRONG ID!";
 
   $file = "{$id}.mapcss";
   // in case we re-initialized the database, compile again
-  $f=adv_exec("pgmapcss -dskunk -uskunk -pPASSWORD -Hlocalhost -tmapnik-2.2 \"{$file}\"", "data/", array());
+  $f=adv_exec("{$pgmapcss['path']} -d'{$db['database']}' -u'{$db['user']}' -p'{$db['password']}' -H'{$db['host']}' -t'{$pgmapcss['template']}' '{$file}'", $data_dir, array());
 
-  return file_get_contents("data/{$file}");
+  return file_get_contents("{$data_dir}/{$file}");
 }
