e8442b8653abe26011d146c41d7f8fe19edd235c
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-03-04 20:55:46 +0100

    Render: write start/stop times to database

--- render.php
+++ render.php
@@ -1,9 +1,16 @@
 <?php include "conf.php"; /* load a local configuration */ ?>
 <?php include "modulekit/loader.php"; /* loads all php-includes */ ?>
 <?
+
+db_check_table('render', 'CREATE TABLE render ( job_id text, style text, bbox text, scale float, timestamp_submit text, timestamp_start text, timestamp_finish text )');
+
 $param = unserialize($argv[1]);
+
+$stat->query("insert into render values ( '{$param['job_id']}', '{$param['style']}', '{$param['bbox']}', '{$param['scale']}', STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW'), STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW'), null )");
 
 chdir($data_dir);
 shell_exec("mapnik-render-image -b {$param['bbox']} --scale={$param['scale']} -o {$param['job_id']} {$param['style']}.mapnik 2>&1 >{$param['job_id']}.debug");
+
+$stat->query("update render set timestamp_finish = STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW') where job_id = '{$param['job_id']}'");
 
 return $job_id;
